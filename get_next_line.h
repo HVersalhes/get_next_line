fdb90b02141f2b34b85227731ebba9d69d5381c1
hversalhes <helderversalhes@gmail.com>
2025-10-05 20:34:34 +0100

    get_next_line.h v1

---
+++ get_next_line.h
@@ -0,0 +1,9 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h> // para read(), close()
+# include <stdlib.h> // para malloc() e free()
+# include <stddef.h> // para uso do size_t
+
+size_t	ft_strlen(const char *s);
+#endif
